8bde2283d2a89ecd17f024c908abe9075a1ebac5
animeshk08 <animuz111@gmail.com>
2019-02-02 03:28:51 +0530

    permissions added

--- Bargain/test1/app/src/main/java/com/example/ayush/finalapp/WelcomePage.java
+++ Bargain/test1/app/src/main/java/com/example/ayush/finalapp/WelcomePage.java
@@ -1,8 +1,13 @@
 package com.example.ayush.finalapp;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,6 +33,12 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        int Permission_All = 1;
+
+        String[] Permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, };
+        if(!hasPermissions(this, Permissions)){
+            ActivityCompat.requestPermissions(this, Permissions, Permission_All);
+        }
 
         firebaseAuth =FirebaseAuth.getInstance();
        user =firebaseAuth.getCurrentUser();
@@ -73,5 +84,18 @@ public class WelcomePage extends AppCompatActivity {
             }
         });
     }
+
+    public static boolean hasPermissions(Context context, String... permissions){
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && context!=null && permissions!=null){
+            for(String permission: permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission)!= PackageManager.PERMISSION_GRANTED){
+                    return  false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
 
