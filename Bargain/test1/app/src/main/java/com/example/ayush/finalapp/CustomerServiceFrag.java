7a22c464f91cb9ed46e4fe38f7ed5d440af7a475
animeshk08 <animuz111@gmail.com>
2019-03-10 03:55:58 +0530

    asd

--- Bargain/test1/app/src/main/java/com/example/ayush/finalapp/CustomerServiceFrag.java
+++ Bargain/test1/app/src/main/java/com/example/ayush/finalapp/CustomerServiceFrag.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,13 @@ public class CustomerServiceFrag extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated (view, savedInstanceState);
-        //msearchview=(SearchView) view.findViewById(R.id.search);
+
+        CardView address= (CardView)view.findViewById(R.id.cs_address);
+        CardView email= (CardView)view.findViewById(R.id.cs_email);
+        CardView phone= (CardView)view.findViewById(R.id.cs_phone);
+
+        phone
+
     }
 
 }
