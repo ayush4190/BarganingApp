ca4373b571851f021afc59eae705c83bbc54fa3d
mickey4190 <a.ayushkumar1997@gmail.com>
2019-03-16 02:05:43 +0530

    session manager enabled

--- Bargain/test1/app/src/main/java/com/example/ayush/finalapp/MainActivity.java
+++ Bargain/test1/app/src/main/java/com/example/ayush/finalapp/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.ayush.finalapp;
 
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    SessionManagment session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /**
+         * Call this function whenever you want to check user login
+         * This will redirect user to LoginActivity is he is not
+         * logged in
+         * */
 
 
        Intent intent = new Intent(this,WelcomePage.class);
