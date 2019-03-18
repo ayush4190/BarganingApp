7fe7f9b7217a1726560adf1912d6cc26976270b0
animeshk08 <animuz111@gmail.com>
2019-03-18 20:14:42 +0530

    community favouriteempty view added

--- Bargain/test1/app/src/main/java/com/example/ayush/finalapp/ShopperDetails.java
+++ Bargain/test1/app/src/main/java/com/example/ayush/finalapp/ShopperDetails.java
@@ -5,20 +5,20 @@ import java.io.Serializable;
 
 public class ShopperDetails implements Serializable {
 
-        String fname , lname , email, dob, phno ,Amount;
+    String fname, lname, email, dob, phno, amount;
 
-        public ShopperDetails(String fname, String lname, String email, String dob, String phno, String Amount) {
-            this.fname = fname;
-            this.lname = lname;
-            this.email = email;
-            this.dob=dob;
-            this.phno=phno;
-            this.Amount = Amount;
+    public ShopperDetails(String fname, String lname, String email, String dob, String phno, String amount) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.dob = dob;
+        this.phno = phno;
+        this.amount = amount;
 
-        }
+    }
 
-        public ShopperDetails() {
-        }
+    public ShopperDetails() {
+    }
 
     public String getDob() {
         return dob;
@@ -37,34 +37,34 @@ public class ShopperDetails implements Serializable {
     }
 
     public String getFname() {
-            return fname;
-        }
+        return fname;
+    }
 
-        public void setFname(String fname) {
-            this.fname = fname;
-        }
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
 
-        public String getLname() {
-            return lname;
-        }
+    public String getLname() {
+        return lname;
+    }
 
-        public void setLname(String lname) {
-            this.lname = lname;
-        }
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getAmount() {
-        return Amount;
+        return amount;
     }
 
     public void setAmount(String amount) {
-        Amount = amount;
+        this.amount = amount;
     }
 }
